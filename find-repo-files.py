f2db79baeb9f864ee8cd5c754acbfac40bd349aa
Bill McCloskey <billm@mozilla.com>
2016-07-17 20:18:30 -0700

    Initial work for comm-central

---
+++ find-repo-files.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+import os
+import os.path
+import sys
+import subprocess
+import json
+
+from lib import run
+
+config = json.load(open(sys.argv[1]))
+tree_name = sys.argv[2]
+
+repo_path = config['trees'][tree_name]['files_path']
+
+if tree_name == 'comm-central':
+    files1 = run('hg locate', shell=True, cwd=repo_path)
+    lines1 = files1.split('\n')
+    files2 = run('hg locate', shell=True, cwd=os.path.join(repo_path, 'mozilla'))
+    lines2 = files2.split('\n')
+
+    lines2 = [ 'mozilla/' + f for f in lines2 if f ]
+
+    lines = lines1 + lines2
+else:
+    stdout = run('git ls-files', shell=True, cwd=repo_path)
+    lines = stdout.split('\n')
+
+files = []
+js = []
+idl = []
+dirs = []
+dirDict = {}
+
+for line in lines:
+    path = line.strip()
+    if not path:
+        continue
+
+    fullpath = os.path.join(repo_path, path)
+
+    elts = path.split('/')
+    for i in range(len(elts)):
+        sub = '/'.join(elts[:i])
+        if sub and sub not in dirDict:
+            dirDict[sub] = True
+            dirs.append(sub + '\n')
+
+    files.append(path + '\n')
+
+    (_, ext) = os.path.splitext(path)
+    if ext == '.idl':
+        # This file causes problems because an IDL file of the same
+        # name exists in browser/, android/, and other places, and
+        # they all end up in dist/include.
+        if not path.endswith('nsIShellService.idl'):
+            idl.append(path + '\n')
+
+    if 'js/src/tests' in path or 'jit-test' in path:
+        continue
+
+    if ext in ['.js', '.jsm', '.xml']:
+        js.append(path + '\n')
+
+index_path = config['trees'][tree_name]['index_path']
+open(os.path.join(index_path, 'repo-files'), 'w').writelines(files)
+open(os.path.join(index_path, 'repo-dirs'), 'w').writelines(dirs)
+open(os.path.join(index_path, 'js-files'), 'w').writelines(js)
+open(os.path.join(index_path, 'idl-files'), 'w').writelines(idl)
+
