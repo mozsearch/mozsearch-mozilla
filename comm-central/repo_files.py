0df5f01531edbdd6db5450772ff6986cb692fe2a
Kartikaya Gupta <kgupta@mozilla.com>
2020-07-07 06:20:59 -0400

    Bug 1616362 - Replace the monolithic per-repo find-repo-files script with only the necessary extension functions.
    This removes the old find-repo-files.py script which was shared by all the repos,
    and special-cased for comm-central. Now we have smaller files that override
    the default behaviour for specific repos that desire to do so.

---
+++ comm-central/repo_files.py
@@ -0,0 +1,19 @@
+import os
+from lib import run
+
+def modify_file_list(files, config=None, **kwargs):
+    # Also grab the file list from the mozilla/ subrepo
+    subrepo_path = os.path.join(config['files_path'], 'mozilla')
+    sub_files = run(['git',  'ls-files'], cwd=subrepo_path).splitlines()
+    sub_files = [ b'mozilla/' + f for f in sub_files if f ]
+    return files + sub_files
+
+def filter_ipdl(path):
+    if 'ipc/ipdl/test' in path:
+        return False
+    return True
+
+def filter_js(path):
+    if 'js/src/tests' in path or 'jit-test' in path:
+        return False
+    return True
