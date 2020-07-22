249747c7a7f4c970e64d8fd79d961241357582bf
Andrew Sutherland <asutherland@asutherland.org>
2020-07-21 22:47:16 -0400

    Bug 1653986 - Download test info for gecko-dev builds and define a wpt_root for m-c. r=kats

--- shared/fetch-tc-artifacts.sh
+++ shared/fetch-tc-artifacts.sh
@@ -15,7 +15,16 @@ INDEXED_HG_REV=$2
 PREEXISTING_HG_REV=$3
 
 # Allow caller to override what we use to download, but
-# have a sane default
+# have a sane default:
+# -s AKA --silent: No progress or error messages unless...
+# -S AKA --show-error: Show an error message despite --silent
+# -f AKA --fail: Emit nothing to standard out on server errors (which would
+#                usually be a boring HTML page), returning error code 22.
+# -L AKA --location: Follow redirects (using the "Location" header)
+#
+# Want it to be an optional call that doesn't fail the build if the file isn't
+# there?  Then try:
+# ${CURL} url -o outut-location || true
 CURL=${CURL:-"curl -SsfL --compressed"}
 
 # Rewrite REVISION to be a specific revision in case the "latest" pointer changes while
@@ -38,6 +47,8 @@ fi
 # we're indexing. But do them in parallel by emitting all the curl commands into
 # a file and then feeding it to GNU parallel.
 echo "${CURL} https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/gecko.v2.$REVISION.source.source-bugzilla-info/artifacts/public/components-normalized.json > bugzilla-components.json" > downloads.lst
+echo "${CURL} https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/gecko.v2.$REVISION.source.test-info-all/artifacts/public/test-info-all-tests.json -o test-info-all-tests.json || true" >> downloads.lst
+echo "${CURL} https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/gecko.v2.$REVISION.source.source-wpt-metadata-summary/artifacts/public/summary.json -o wpt-metadata-summary.json || true" >> downloads.lst
 for PLATFORM in linux64 macosx64 win64 android-armv7; do
     TC_PREFIX="https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/gecko.v2.${REVISION}.firefox.${PLATFORM}-searchfox-debug/artifacts/public/build"
     # First check that the searchfox job exists for the platform and revision we want. Otherwise emit a warning and skip it. This
