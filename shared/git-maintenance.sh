0bab7f272f5641031c63aaa570940a9e1616f116
Kartikaya Gupta <kats@trevize.staktrace.com>
2021-02-06 21:50:36 -0500

    Bug 1691239 - Run git gc prior to repo uploads on saturday AM runs.

---
+++ shared/git-maintenance.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -x # Show commands
+set -eu # Errors/undefined vars are fatal
+set -o pipefail # Check all commands in a pipeline
+
+GIT_REPO_DIR="$1"
+
+if [[ $(date +%A) == "Saturday" ]]; then
+    date
+    git --git-dir="${GIT_REPO_DIR}/.git" gc
+    date
+fi
