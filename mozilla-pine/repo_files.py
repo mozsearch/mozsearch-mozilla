32eefbbcb8767cb5a7c66051b59df011d747d4fa
Kartikaya Gupta <kats@trevize.staktrace.com>
2023-07-01 11:55:57 -0400

    Bug 1841395 - Remove pine branch

--- mozilla-pine/repo_files.py
+++
@@ -1,9 +0,0 @@
-def filter_ipdl(path):
-    if 'ipc/ipdl/test' in path:
-        return False
-    return True
-
-def filter_js(path):
-    if 'js/src/tests' in path or 'jit-test' in path:
-        return False
-    return True
